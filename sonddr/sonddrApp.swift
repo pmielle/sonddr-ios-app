37008d1c557c9123190b2b284f0b8c1b1eda9905
Paul Mielle <pm.mielle@gmail.com>
2022-06-20 22:48:34 +0200

    Initial Commit

---
+++ sonddr/sonddrApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonddrApp.swift
+//  sonddr
+//
+//  Created by Paul Mielle on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct sonddrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
